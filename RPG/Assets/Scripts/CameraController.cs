0490219bc9525ad6c8a5ce41f1aa9a9192eedb40
LTPStudio <ltpstudioxr@gmail.com>
2021-05-16 04:19:01 -0700

    daily commit

---
+++ RPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float pitch = 2f;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f, maxZoom = 15f;
+    public float yawSpeed = 100f; 
+
+    private float currentYaw = 0f; 
+    float currentZoom = 10f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw += Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+    }
+}
---
+++ RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    Camera cam;
+    PlayerMotor motor;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit; 
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("We hit " + hit.collider.name + " at " + hit.point);
+                //move player to what we hit 
+                motor.MoveToPoint(hit.point);
+                //stop focusing any objects 
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //check if we hit an interactable 
+                //if we did set it as our focus
+            }
+        }
+    }
+}
